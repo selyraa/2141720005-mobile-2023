351add6e244e3ab19d4df9a910ec6bf988d716f4
selyraa <110004540+selyraa@users.noreply.github.com>
2023-09-11 10:19:38 +0700

    add praktikum week 3
    add praktikum week 3

---
+++ week-03/src/praktikum-01.dart
@@ -0,0 +1,19 @@
+void main(){
+  bool test = true;
+  if (test){
+    print("Kebenaran");
+  } else {
+    print("Kesalahan");
+  }
+  // String test = "test2";
+  // if (test == "test1"){
+  //   print("Test1");
+  // } else if (test == "test2"){
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+  // if (test == "test2") print ("Test2 again");
+
+  
+}
---
+++ week-03/src/praktikum-02.dart
@@ -0,0 +1,11 @@
+void main(){
+  int counter =80;
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+  // while (counter < 33) {
+  //   print(counter);
+  //   counter++;
+  // }
+}
